e3e7eaaea5703e07f33e7be1f748b74b8f92695a
josephyao <dermott@126.com>
2013-06-28 23:58:26 +0800

    Regular customer reserve Saturday

--- src/main/java/HotelReservation.java
+++ src/main/java/HotelReservation.java
@@ -2,6 +2,9 @@
 public class HotelReservation {
 
 	public String reserve(String customerAndDates) {
+		if (customerAndDates.contains("sat"))
+			return "Bridgewood";
+			
 		return "Lakewood";
 	}
 
--- src/test/java/TestHotelReservation.java
+++ src/test/java/TestHotelReservation.java
@@ -5,11 +5,21 @@ import org.junit.Test;
 
 public class TestHotelReservation {
 
+	private HotelReservation hotelReservationHelper = new HotelReservation();
+	
 	@Test
 	public void regular_customer_reserve_monday() {
-		HotelReservation hotelReservationHelper = new HotelReservation();
-		String actualReservedHotel = hotelReservationHelper.reserve("Regular: 16Mar2009(mon)");
-		assertEquals("Lakewood", actualReservedHotel);
+		assertReservedHotelEquals("Lakewood", "Regular: 24Jun2013(mon)");
 	}
 
+	@Test
+	public void regular_customer_reserve_saturday() {
+		assertReservedHotelEquals("Bridgewood", "Regular: 29Jun2013(sat)");
+	}
+
+	private void assertReservedHotelEquals(String expectedReservedHotel, String customerAndDates) {
+		String actualReservedHotel = hotelReservationHelper.reserve(customerAndDates);
+		assertEquals(expectedReservedHotel, actualReservedHotel);
+	}
+	
 }
