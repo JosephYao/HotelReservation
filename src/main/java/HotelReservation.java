2ad7418213e5707bae4cc3d6766cf604eb1315ad
josephyao <dermott@126.com>
2013-06-29 00:06:57 +0800

    Rewards customer reserve one weekend

--- src/main/java/HotelReservation.java
+++ src/main/java/HotelReservation.java
@@ -2,6 +2,9 @@
 public class HotelReservation {
 
 	public String reserve(String customerAndDates) {
+		if (customerAndDates.startsWith("Rewards:"))
+			return "Ridgewood";
+		
 		if (customerAndDates.contains("sat"))
 			return "Bridgewood";
 			
--- src/test/java/TestHotelReservation.java
+++ src/test/java/TestHotelReservation.java
@@ -8,13 +8,18 @@ public class TestHotelReservation {
 	private HotelReservation hotelReservationHelper = new HotelReservation();
 	
 	@Test
-	public void regular_customer_reserve_monday() {
+	public void regular_customer_reserve_one_weekday() {
 		assertReservedHotelEquals("Lakewood", "Regular: 24Jun2013(mon)");
 	}
 
 	@Test
-	public void regular_customer_reserve_saturday() {
+	public void regular_customer_reserve_one_weekend() {
 		assertReservedHotelEquals("Bridgewood", "Regular: 29Jun2013(sat)");
+	}
+	
+	@Test
+	public void rewards_customer_reserve_one_weekend() {
+		assertReservedHotelEquals("Ridgewood", "Rewards: 29Jun2013(sat)");
 	}
 
 	private void assertReservedHotelEquals(String expectedReservedHotel, String customerAndDates) {
